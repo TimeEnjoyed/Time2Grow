a48d2a7a6901f09281657af8d8c96d2c3316fca6
EvieePy <29671945+EvieePy@users.noreply.github.com>
2026-02-02 14:27:50 +1000

    Base design for the GameAdapter (Server)

--- time2grow/adapter.py
+++ time2grow/adapter.py
@@ -24,21 +24,99 @@ SOFTWARE.
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import asyncio
+import json
+from collections import defaultdict
+from typing import TYPE_CHECKING, cast
 
+from sse_starlette import EventSourceResponse
+from starlette.responses import FileResponse, Response
+from starlette.staticfiles import StaticFiles
 from twitchio import web
 
 
 if TYPE_CHECKING:
+    from collections.abc import AsyncGenerator
+
     from starlette.requests import Request
 
     from .bot import Bot
+    from .components.game import GameComponent, GameLoop
+    from .plants import Plant
+    from .types_ import DispatchDataT
 
 
 class GameAdapter(web.StarletteAdapter["Bot"]):
     def __init__(self, *, host: str, port: int, client: Bot, domain: str | None = None) -> None:
         super().__init__(host=host, port=port, client=client, domain=domain)
+        self.queues: dict[str, set[asyncio.Queue[DispatchDataT]]] = defaultdict(set)
 
-    async def sse_endpoint(self, request: Request) -> ...: ...
+        self.add_route("/overlays/{identifier}", self.game_overlay, methods=["GET"])
+        self.add_route("/overlays/{identifier}/sse", self.sse_endpoint, methods=["GET"])
+        self.mount("/static", app=StaticFiles(directory="static", html=False), name="static")
 
-    async def game_overlay(self, request: Request) -> ...: ...
+    async def dispatch(self, game: GameLoop, *, plants: set[Plant]) -> None:
+        queues = self.queues[game.overlay_id]
+        data: DispatchDataT = {"plants": [p.to_send() for p in plants], "fresh": False, "positions": game.sort_positions()}
+
+        for queue in queues:
+            await queue.put(data)
+
+    async def sse_source(
+        self,
+        request: Request,
+        *,
+        queue: asyncio.Queue[DispatchDataT],
+        broadcaster: str,
+    ) -> AsyncGenerator[str]:
+        # TODO: Better error...
+        component: GameComponent = cast("GameComponent", self.client.get_component("GameComponent"))
+        if not component:
+            raise RuntimeError
+
+        user = self.client.create_partialuser(user_id=broadcaster)
+        game = component.games.get(user)
+
+        if game:
+            data: DispatchDataT = {
+                "plants": [p.copy().to_send() for p in game._plants.values()],
+                "fresh": True,
+                "positions": game.sort_positions(),
+            }
+            yield json.dumps(data)
+
+        while True:
+            try:
+                data: DispatchDataT = await queue.get()
+                yield json.dumps(data)
+            except asyncio.CancelledError:
+                break
+
+            if await request.is_disconnected():
+                break
+
+        identifier: str = request.path_params["identifier"]
+        self.queues[identifier].discard(queue)
+
+    async def sse_endpoint(self, request: Request) -> EventSourceResponse | Response:
+        identifier: str = request.path_params["identifier"]
+
+        model = await self.client.db.fetch_broadcaster_by_overlay(identifier)
+        if not model:
+            return Response(status_code=404)
+
+        self.client.overlays[model.uid] = identifier
+
+        queue: asyncio.Queue[DispatchDataT] = asyncio.Queue()
+        self.queues[identifier].add(queue)
+
+        return EventSourceResponse(self.sse_source(request, queue=queue, broadcaster=model.uid))
+
+    async def game_overlay(self, request: Request) -> Response:
+        identifier: str = request.path_params["identifier"]
+
+        model = await self.client.db.fetch_broadcaster_by_overlay(identifier)
+        if not model:
+            return Response(content="The provided overlay key is not valid", status_code=404)
+
+        return FileResponse("static/index.html", media_type="text/html", content_disposition_type="inline")
